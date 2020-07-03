7894b9590929bb3847455d38dc2efa010a48fab8
korotko <marekdziarski@gmail.com>
2020-07-03 08:40:35 +0200

    Clean data input

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('0.0.0.0', 8080))
 serv.listen(5)
@@ -8,7 +9,10 @@ while True:
     while True:
         data = conn.recv(4096)
         if not data: break
-        print(data)
-
+        print(str(data))
+        clean_data = data[:-2]
+        print(clean_data)
+        split_data = clean_data.decode().split(",")
+        print(split_data)
     conn.close()
     print('client disconnected')
